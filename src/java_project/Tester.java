c0d4b4f0a00a6033306daab60e4ab92b4438f3f0
marmpa <marmpa@gmail.com>
2017-12-20 14:33:59 +0200

    Everything works

--- src/java_project/Tester.java
+++ src/java_project/Tester.java
@@ -37,49 +37,56 @@ public class Tester {
         
         
         
-        Date from, to;
-        Calendar calendar = Calendar.getInstance();
+        Date from, to;//Δημιουργώ ένα νέο αντικείμενο τύπου Date
+        Calendar calendar = Calendar.getInstance();//καλώ την Calendars.getInstance() ώστε να δώσει μία τυπική ημερομηνία στο Calendar
 
-        calendar.set(2017, 11, 20);
-        from = calendar.getTime();
-        calendar.set(2017, 11, 27);
-        to = calendar.getTime();
+        calendar.set(2017, 11, 20);//θέτει την ημερομηνία του calendar σε αυτη στην παρενθεση
+        from = calendar.getTime();//παίρνει την ημερομηνία απο την μεταβλητη calendar την μετατρεπη σε τύπου Date και την αποθηκεύη στη from
+        calendar.set(2017, 11, 27);//θέτει την ημερομηνία του calendar σε αυτη στην παρενθεση
+        to = calendar.getTime();//παίρνει την ημερομηνία απο την μεταβλητη calendar την μετατρεπη σε τύπου Date και την αποθηκεύη στη to
         
         hotel.NewReservation("1", "Marios", from, to, vehicles.get(2));
         
-         calendar.set(2017, 11, 25);
-        from = calendar.getTime();
-        calendar.set(2017, 11, 29);
-        to = calendar.getTime();
+         calendar.set(2017, 11, 25);//παρομοίος
+        from = calendar.getTime();//παρομοίος
+        calendar.set(2017, 11, 29);//παρομοίος
+        to = calendar.getTime();//παρομοίος
         
         hotel.NewReservation("2", "Giota", from, to, rooms.get(0));
         
-        calendar.set(2017, 11, 18);
-        from = calendar.getTime();
-        calendar.set(2017, 11, 25);
-        to = calendar.getTime();
+        calendar.set(2017, 11, 18);//παρομοίος
+        from = calendar.getTime();//παρομοίος
+        calendar.set(2017, 11, 25);//παρομοίος
+        to = calendar.getTime();//παρομοίος
         
         hotel.NewReservation("3", "Panos", from, to, rooms.get(1));
         
-        calendar.set(2017, 11, 20);
-        from = calendar.getTime();
-        calendar.set(2017, 11, 29);
-        to = calendar.getTime();
+        calendar.set(2017, 11, 20);//παρομοίος
+        from = calendar.getTime();//παρομοίος
+        calendar.set(2017, 11, 29);//παρομοίος
+        to = calendar.getTime();//παρομοίος
         
         hotel.NewReservation("4", "Tom", from, to, rooms.get(2));
         
-        calendar.set(2017, 11, 20);
-        from = calendar.getTime();
-        calendar.set(2017, 11, 29);
-        to = calendar.getTime();
+        calendar.set(2017, 11, 20);//παρομοίος
+        from = calendar.getTime();//παρομοίος
+        calendar.set(2017, 11, 29);//παρομοίος
+        to = calendar.getTime();//παρομοίος
         
         hotel.NewReservation("5", "Greg", from, to, rooms.get(3));
         
         Date betw;
-        calendar.set(2017, 11, 25);
-        betw=calendar.getTime();
+        calendar.set(2017, 11, 25);//παρομοίος
+        betw=calendar.getTime();//παρομοίος
         System.out.println(hotel.Search_code("5"));//δουλευει
-        System.out.println(hotel.Search_date(betw));//!!!!εδω δεν επιστρέφει λίστα αλλά μόνο ένα στοιχείο!!!!
+        
+        System.out.println("\nReservations:");
+        for(Reservation reservation:hotel.Search_date(betw))
+        {
+            System.out.println(reservation);
+            System.out.println();
+        }
+        System.out.println("End of reservations!!!");
         System.out.println(hotel.Search_name("Tom"));//δουλευει
         hotel.Delete_reservation("2");//δουλευει
         hotel.UniqueTypes(betw);//δουλευει
